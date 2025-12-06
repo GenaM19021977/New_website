616dc63b79a84be2f22c380534cb508c54f25111
GennadiJ Malchewski <you@example.com>
2025-12-06 14:33:30 +0300

    прописал URL нашего сервера в курсах

--- backend/electric_boiler/settings.py
+++ backend/electric_boiler/settings.py
@@ -26,10 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'products',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -37,6 +39,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = [
+    'http://localhost:5173'
 ]
 
 ROOT_URLCONF = 'electric_boiler.urls'
